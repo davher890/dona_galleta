13f4862fc049a23447b532c4ddd9b99abaf6e75e
d.hernando <d.hernando@mnemo.com>
2016-12-22 10:24:56 +0100

    Slider in the modal view

--- public/js/imageModal.js
+++ public/js/imageModal.js
@@ -3,34 +3,49 @@
  */
 
 
-function showModal(img) {
+function showModal(data) {
 
     // Get the modal
-    var modal = document.getElementById('myModal');
+    var modal = $('#myModal');
+
+    var oldCarousel = $('#myModal').find('ol.carousel-indicators');
+    var carouselInner = $('#myModal').find('.carousel-inner');
+
+    var info = JSON.parse($(data).attr('data-data'));
+    var imgs = info.img;
+
+    for (var i = 0; i < imgs.length; i++) {
+        if (i == 0) {
+            oldCarousel.append("<li class='active' data-target='#carousel-example-1' data-slide-to='" + i + "' />");
+            carouselInner.append("<div class='carousel-item active'><img style='margin:auto' src='" + imgs[i] + "'/></div>");
+        } else {
+            oldCarousel.append("<li data-target='#carousel-example-1' data-slide-to='" + i + "' />");
+            carouselInner.append("<div class='carousel-item'><img style='margin:auto' src='" + imgs[i] + "'/></div>");
+        }
+
+    }
 
     // Get the image and insert it inside the modal - use its "alt" text as a caption
-    // var img = document.getElementById('myImg');
-    var modalImg = document.getElementById("img01");
-    var captionText = document.getElementById("caption");
-    modal.style.display = "block";
-    modalImg.src = img.src;
-    captionText.innerHTML = img.alt;
+    var modalImg = $("#img01");
+    var captionText = $("#caption");
+    modal.css('display', 'block');
+    modalImg.attr('src', data.src);
+    captionText.html(data.alt);
 
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    var span = $(".close");
 
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
-        modal.style.display = "none";
-    }
+    span.click(function() {
+        modal.css('display', 'none');
+    });
 }
 
 
-function changeContactFormVisibility() {
+function showContactForm() {
     if ($('#contactForm').css('visibility') === 'hidden') {
         $('#contactForm').css('visibility', 'visible')
-    }
-    else {
+    } else {
         $('#contactForm').css('visibility', 'hidden')
     }
 }
